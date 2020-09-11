d7db9c751b1bd1275a7a2ee16f1b3c474163b836
Mulher mão no código <71080977+mulhermaonocodigo@users.noreply.github.com>
2020-09-10 23:12:53 -0300

    primeiro upload
    Realiza as 4 operações aritméticas básicas.

---
+++ operacoes_aritmeticas.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 20:19:14 2020
+
+@author: mm
+
+descrição: este programa realiza operações de soma,  multiplicação,  divisão e subtração
+"""
+
+valor_a = 20
+valor_b = 7
+
+resultado_soma = valor_a + valor_b
+
+print(str(resultado_soma))
+
+#multiplicação
+
+valor_c = 80
+valor_d = 2
+
+resultado_multiplicacao = valor_c * valor_d
+
+print(str(resultado_multiplicacao))
+#divisão
+
+valor_e = 30
+valor_f = 3
+
+resultado_divisao = valor_e / valor_f
+
+print(str(resultado_divisao))
+
+#divisão
+
+valor_g = 10
+valor_h = 7
+
+resultado_subtracao = valor_g - valor_h
+
+print(str(resultado_subtracao))
