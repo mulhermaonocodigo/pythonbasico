0868e22527493fd8671bce57fe8a8585164f828c
Mulher mão no código <71080977+mulhermaonocodigo@users.noreply.github.com>
2020-09-21 23:57:59 -0300

    primeiro upload
    Vários exemplos de uso do if, estrutura de condição.

---
+++ condicao.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 23:00:21 2020
+
+@author: mm
+"""
+sua_idade = 18
+
+
+if sua_idade>=18 :
+     
+
+	print("Parabéns! Você pode tirar a carteira de motorista. ")
+     
+
+else:
+
+	print("Desculpe, mas infelizmente você ainda não pode tirar carteira de motorista.")
+#------------------------------------
+salario = 4000
+
+bonus = 0
+
+
+if (salario < 5000) :
+	
+     bonus   = salario *(3/100)
+     
+     salario = salario + bonus
+
+print("Seu bônus será de:", bonus, "e seu novo salário será", salario)
+
+#------------------------------------
+numero_filhos = 2
+
+rendimento_anual= 35.00000
+
+
+if rendimento_anual < 28.55970 and numero_filhos > 3:
+
+	print("Você tem direito a um bônus família."  )
+
+else:
+
+	print("O bônus família não está disponível.")
+
+    
