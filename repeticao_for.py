d8533c27d79235c6e00ad903db2a3ecd9a4c76ae
Mulher mão no código <71080977+mulhermaonocodigo@users.noreply.github.com>
2020-10-17 23:26:29 -0300

    primeiro upload
    Exemplos usando estrutura de repetição for

---
+++ repeticao_for.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 11 21:53:53 2020
+
+@author: mm
+"""
+
+for i in range(1,6):
+    print(i)
+ 
+#----------------------
+lista_frutas=['maça', 'banana', 'manga']
+
+
+for fruta in lista_frutas :
+    print(fruta)
+    
+#----------------------
+
+for i in range(0,len(lista_frutas)):
+    print(lista_frutas[i], "foi adicionada à lista.")
+    
+
